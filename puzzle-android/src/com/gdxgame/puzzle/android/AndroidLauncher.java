8d0f8cc0b6c66727adff7331ffba78cf934b0166
guyyo <owner@owner-HP>
2014-04-26 15:16:22 +0300

    first commit

---
+++ puzzle-android/src/com/gdxgame/puzzle/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.gdxgame.puzzle.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.gdxgame.puzzle.Puzzle;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Puzzle(), config);
+	}
+}
